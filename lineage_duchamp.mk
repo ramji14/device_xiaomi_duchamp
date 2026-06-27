75c9d6774bff61bb9eb7eb2122ae8df3ff2a42b8
ramji14 <132249784+ramji14@users.noreply.github.com>
2026-06-27 14:56:09 +0530

    Update lineage_duchamp.mk

--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -13,7 +13,7 @@ $(call inherit-product, device/xiaomi/duchamp/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_duchamp
+PRODUCT_NAME := bliss_duchamp
 PRODUCT_DEVICE := duchamp
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
